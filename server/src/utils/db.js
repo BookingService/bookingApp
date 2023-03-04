02fd3c49241c2f59c56130d9fafd7a52efb3c715
Igor <azawertik@gmail.com>
2023-03-04 13:00:41 +0300

    feature/BCSC-1 added folders structure

---
+++ server/src/utils/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+const db = mongoose.connection;
+
+export default db;
