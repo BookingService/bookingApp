d25447847f1c52a203c80b0368fbedb1e5c08286
Igor <azawertik@gmail.com>
2023-03-05 22:36:09 +0300

    BCSC-3 added schemas

---
+++ server/src/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Date,
+      required: true,
+    },
+    favorites: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Flat",
+      },
+    ],
+    bookings: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Booking",
+      },
+    ],
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", UserSchema);
