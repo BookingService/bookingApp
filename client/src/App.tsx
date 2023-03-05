3a1e51464c535b3815ab8bcfd34397b40d8625e6
Igor <azawertik@gmail.com>
2023-03-05 22:46:29 +0300

    BCSC-2 initialize project, added some folders and create empty colors.css for colors variables

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import "./index.css";
+function App() {
+  return (
+    <div className="bg-blue-500 h-screen">
+      <h1 className="text-black text-1xl">Hello Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
