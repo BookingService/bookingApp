8db54c66f09b6ce2bea5401251e19ccb43ab424f
azawert <53710085+azawert@users.noreply.github.com>
2023-03-08 01:37:24 +0300

    BCSC-7 fixed let to const

--- server/src/middleware/authenicate.js
+++ server/src/middleware/authenicate.js
@@ -1,11 +1,10 @@
 import { messages } from "../utils/text.js";
 import jwt from "jsonwebtoken";
 export const isUserAuthenicated = (req, res, next) => {
-  let token;
   if (!req.headers.authorization) {
     return res.status(403).json({ message: messages[403] });
   }
-  token = req.headers.authorization.slice(7);
+  const token = req.headers.authorization.slice(7);
   if (!token) {
     return res.status(403).json({ message: messages[403] });
   }
