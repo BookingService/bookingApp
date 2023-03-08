3f690a4383a68191ef8c767cf53c34cd68e48a22
azawert <53710085+azawert@users.noreply.github.com>
2023-03-08 19:07:21 +0300

    BCSC-7 added auth middleware

--- server/src/middleware/authenicate.js
+++ server/src/middleware/authenicate.js
@@ -1,13 +1,10 @@
 import { messages } from "../utils/text.js";
 import jwt from "jsonwebtoken";
-export const isUserAuthenicated = (req, res, next) => {
+export const isUserAuthenticated = (req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(403).json({ message: messages[403] });
   }
   const token = req.headers.authorization.slice(7);
-  if (!token) {
-    return res.status(403).json({ message: messages[403] });
-  }
   try {
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.user = decoded;
