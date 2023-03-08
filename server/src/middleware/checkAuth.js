296fee59d4e9daacde3fcd949450437202b4bafe
azawert <53710085+azawert@users.noreply.github.com>
2023-03-08 20:26:59 +0300

    BSCS-7 added auth middleware

---
+++ server/src/middleware/checkAuth.js
@@ -0,0 +1,15 @@
+import { messages } from "../utils/text.js";
+import jwt from "jsonwebtoken";
+export const checkAuth = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ message: messages[403] });
+  }
+  const token = req.headers.authorization.slice(7);
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(403).json({ message: messages[403] });
+  }
+};
