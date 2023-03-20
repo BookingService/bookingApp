ef1ce0059bde68e7b2630cd8c9d4fdc5c905af5c
azawert <53710085+azawert@users.noreply.github.com>
2023-03-20 22:04:58 +0300

    bcsc-10 added a auth check before making requests

--- server/src/routes/Auth.js
+++ server/src/routes/Auth.js
@@ -5,6 +5,10 @@ const router = express.Router();
 const authController = new AuthController();
 
 router.post("/login", authController.login);
-router.post("/registration", authController.registrateUser);
+router.post(
+  "/registration",
+
+  authController.registrateUser
+);
 
 export default router;
--- server/src/routes/Review.js
+++ server/src/routes/Review.js
@@ -1,11 +1,12 @@
 import { ReviewController } from "../controllers/Review.js";
 import express from "express";
+import { checkAuth } from "../middleware/checkAuth.js";
 
 const router = express.Router();
 const reviewController = new ReviewController();
 
-router.post("/", reviewController.createReview);
-router.delete("/:id", reviewController.deleteReview);
-router.put("/:id", reviewController.updateReview);
+router.post("/", checkAuth, reviewController.createReview);
+router.delete("/:id", checkAuth, reviewController.deleteReview);
+router.put("/:id", checkAuth, reviewController.updateReview);
 
 export default router;
